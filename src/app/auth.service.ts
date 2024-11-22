fdf9b5232879c17296f3d353b4bcd7b751ab3c69
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2024-11-22 10:31:42 -0600

    Login working with external api

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,25 +1,45 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface User {
+  email: string;
+  password: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements OnInit {
   readonly USERS_ENDPOINT = "https://api.escuelajs.co/api/v1/users"
   private success = false;
+  readonly users: User[] = [];
 
-  constructor(private httpClient: HttpClient, private router: Router) { }
+  constructor(private httpClient: HttpClient) {
+    this.getUsers().subscribe((users: User[]) => {
+      this.users.push(...users)
+      console.log(users);
+    });
+  }
+
+  ngOnInit() {
+
+  }
 
   login(email: string, password: string) {
-    return this.httpClient.get(this.USERS_ENDPOINT).subscribe((users: any) => {
-      const user = users.find((user: any) => user.email === email && user.password === password);
-      if (user) {
-        localStorage.setItem('user', JSON.stringify(user));
-        this.router.navigate(['/dashboard']);
-        this.success = true;
-      }
-      return this.success;
-    })
+    const user = this.users.find((user: any) => user.email === email && user.password === password);
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+      this.success = true;
+    }
+    return this.success;
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.USERS_ENDPOINT);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,23 +12,26 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class  LoginComponent {
   public email = '';
   public password = '';
   public error = ""
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { 
+  }
 
   login() {
+  
     if (this.password.length === 0 || this.email.length === 0) {
       this.error = "Todos los campos son requeridos"
       return
     }
-
     const loggedIn = this.authService.login(this.email, this.password)
 
     if (!loggedIn) {
       this.error = "Credenciales incorrectas"
+    } else {
+      this.router.navigate(['/dashboard'])
     }
   }
 }
